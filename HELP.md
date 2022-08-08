f9f64d16a32b1746641595995b720eaf70d48504
DivaevAM <DivaevAM@intech.rshb.ru>
2022-08-08 14:56:33 +0500

    added population db

---
+++ HELP.md
@@ -0,0 +1,20 @@
+# АРМ контороля посещений учащихся
+
+---
+
+## Описание изменений системы:
+
+1)  * Монолитный проект для взаимодействия со [СКУД "СИГУР"](https://sigur.com/)
+    * [Spring Boot](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/), [Spring Web](https://docs.spring.io/spring-boot/docs/2.7.2/reference/htmlsingle/#web), [Spring Data JPA](https://docs.spring.io/spring-boot/docs/2.7.2/reference/htmlsingle/#data.sql.jpa-and-spring-data), [Lombok](https://projectlombok.org/)
+    * Добавлена база H2
+2)  
+
+
+---
+
+### Работа с базой H2
+#### Получить доступ ка базе H2
+1) Запустить приложение
+2) [http://localhost:8081/h2-console]()
+
+---
--- src/main/java/ru/schoolservice/arm/ArmApplication.java
+++ src/main/java/ru/schoolservice/arm/ArmApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.schoolservice.arm.model.Role;
-import ru.schoolservice.arm.model.User;
 import ru.schoolservice.arm.repository.UserRepository;
-
-import javax.transaction.Transactional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -25,8 +19,8 @@ public class ArmApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Stream.of(Role.ROLE_USER).collect(Collectors.toSet())));
-        userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Stream.of(Role.ROLE_USER, Role.ROLE_ADMIN).collect(Collectors.toSet())));
+        //userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Stream.of(Role.ROLE_USER).collect(Collectors.toSet())));
+        //userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Stream.of(Role.ROLE_USER, Role.ROLE_ADMIN).collect(Collectors.toSet())));
         System.out.println("**************************************findAll******************************************");
         System.out.println(userRepository.findAll());
     }
