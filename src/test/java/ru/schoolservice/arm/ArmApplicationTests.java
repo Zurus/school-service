e7008781063487860c0955b2622b09225587cf30
DivaevAM <DivaevAM@intech.rshb.ru>
2022-11-15 10:01:28 +0500

    fix tests

--- src/test/java/ru/schoolservice/arm/ArmApplicationTests.java
+++ src/test/java/ru/schoolservice/arm/ArmApplicationTests.java
@@ -1,13 +1,29 @@
 package ru.schoolservice.arm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 class ArmApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	protected ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
+		return mockMvc.perform(builder);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
